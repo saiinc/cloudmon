2991d42dc75234323620f379bccc3ed78f2f9af1
Saionji <saiinc@narod.ru>
2022-05-21 10:15:14 +0300

    Update main.py
    remove all filesave

--- main.py
+++ main.py
@@ -7,33 +7,21 @@ import psycopg2
 from psycopg2 import OperationalError
 from psycopg2 import Error
 
-from github import Github
-import csv
-import zipfile
-import base64
-import pyminizip
-
-
 DATABASE_URL = os.environ['DATABASE_URL']
-#NODELIST = os.environ['NODELIST']
+# NODELIST = os.environ['NODELIST']
 SND_PATH = os.environ['SND_PATH']
 STATUS_PATH = os.environ['STATUS_PATH']
 LOG_PATH = os.environ['LOG_PATH']
 TELEGRAM_TOKEN = os.environ['TELEGRAM_TOKEN']
 TLG_CHAT_ID = os.environ['TLG_CHAT_ID']
 PASSPHRASE = os.environ['PASSPHRASE']
-GITHUB_TOKEN = os.environ['GITHUB_TOKEN']
-#GITHUB_USERNAME = os.environ['GITHUB_USERNAME']
-#REPOSITORY_NAME = os.environ['REPOSITORY_NAME']
 
-
+# Подключиться к существующей базе данных
+psycopg2.connect(DATABASE_URL)
+connection = psycopg2.connect(DATABASE_URL, sslmode='require')
+# Создайте курсор для выполнения операций с базой данных
+cursor = connection.cursor()
 try:
-    # Подключиться к существующей базе данных
-    psycopg2.connect(DATABASE_URL)
-    connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-    # Создайте курсор для выполнения операций с базой данных
-    cursor = connection.cursor()
     # SQL-запрос для создания новой таблицы
     create_table_query = '''CREATE TABLE nodelist
                           (node_name     TEXT                NOT NULL,
@@ -52,13 +40,9 @@ finally:
         connection.close()
         print("Соединение с PostgreSQL закрыто")
 
-
 app = Flask(__name__)
 psycopg2.connect(DATABASE_URL)
 connection = psycopg2.connect(DATABASE_URL, sslmode='require')
-g = Github(GITHUB_TOKEN)
-#repo = g.get_repo(GITHUB_USERNAME + '/' + REPOSITORY_NAME)
-repo = g.get_repo("cloudmon1/cloudmon")
 git_file = 'temp.db'
 
 password = str.encode(PASSPHRASE)
@@ -88,42 +72,6 @@ def execute_read_query(connection_db, query):
         print(f"The error '{e}' occurred")
 
 
-def str2bool(v):
-    return v.lower() in ("true", "yes", "t", "1")
-
-
-def save_to_csv(list_nodes, filename):
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = ['node_name', 'alert', 'time']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        for item in list_nodes:
-            writer.writerow(item)
-    pyminizip.compress(filename, "", "temp_db.zip", password, 1)
-
-
-def upload_to_github():
-    all_repo_files = []
-    contents = repo.get_contents("")
-    while contents:
-        file_content = contents.pop(0)
-        if file_content.type == "dir":
-            contents.extend(repo.get_contents(file_content.path))
-        else:
-            file = file_content
-            all_repo_files.append(str(file).replace('ContentFile(path="', '').replace('")', ''))
-
-    with open("temp_db.zip", 'rb') as file:
-        content_to_upload = base64.b64encode(file.read())
-    if "temp_db.zip" in all_repo_files:
-        contents = repo.get_contents("temp_db.zip")
-        print('content_to_upload: ' + str(content_to_upload))
-        print('repo.update_file: ' + str(repo.update_file("temp_db.zip", "committing files", content_to_upload, contents.sha)))
-    else:
-        print('content_to_upload: ' + str(content_to_upload))
-        repo.create_file("temp_db.zip", "committing files", str(content_to_upload.decode()), branch="master")
-        print("temp_db.zip" + ' CREATED')
-
-
 def get_nodes():
     nodes_str = 'node_test, home_test, 123qwe123'
     nodes = tuple(map(str, nodes_str.split(', ')))
@@ -148,19 +96,16 @@ def get_nodes():
         list_nodes.append(dict_dbnode)
     to_add = list(set(nodes) - set(list_dbnodenames))
     print('to_add: ' + str(to_add))
-    db_to_add = []
-    for row in to_add:
-        dict_node = {'node_name': row, 'alert': False, 'time': datetime.now()}
-        list_nodes.append(dict_node)
-        list_node = [row, False, datetime.now()]
-        db_to_add.append(list_node)
-    print('final list_nodes: ' + str(list_nodes))
     if to_add:
         cursor = connection.cursor()
-        for row in db_to_add:
-            cursor.execute("INSERT INTO nodelist (node_name, state, time) VALUES (%s, %s, %s)", (row[0], row[1], row[2]))
+        for row in to_add:
+            cursor.execute("INSERT INTO nodelist (node_name, state, time) VALUES (%s, %s, %s)",
+                           (row, False, datetime.now()))
+            dict_node = {'node_name': row, 'alert': False, 'time': datetime.now()}
+            list_nodes.append(dict_node)
         connection.commit()
-    #if to_delete:
+    print('final list_nodes: ' + str(list_nodes))
+    # if to_delete:
     return list_nodes
 
 
@@ -186,22 +131,24 @@ def state_checker(message, index):
         if message.get('alert') is False:
             message.update({'alert': True})
             print(' '.join(["Status Alert:", str(datetime.now() - message.get('time'))]))
-            save_to_csv(nodelist, git_file)
-            upload_to_github()
+            # save_to_csv(nodelist, git_file)
+            # upload_to_github()
             print(''.join(["Alert message send to Telegram ", sender_tlg(index, True)]))
             return print('Status ' + message.get('node_name') + ' switched to Alert')
         else:
-            return print(' '.join(['Status ' + message.get('node_name') + ' is Alert:', str(datetime.now() - message.get('time'))]))
+            return print(' '.join(
+                ['Status ' + message.get('node_name') + ' is Alert:', str(datetime.now() - message.get('time'))]))
     else:
         if message.get('alert') is True:
             message.update({'alert': False})
-            save_to_csv(nodelist, git_file)
-            upload_to_github()
+            # save_to_csv(nodelist, git_file)
+            # upload_to_github()
             print(''.join(["Alive message send to Telegram ", sender_tlg(index, False)]))
             return print('Status ' + message.get('node_name') + ' switched to OK')
         else:
-            return print(' '.join(['Status ' + message.get('node_name') + ' is OK,', "time now:", str(datetime.now()), "  ",
-                                   "time msg:", str(message.get('time'))]))
+            return print(
+                ' '.join(['Status ' + message.get('node_name') + ' is OK,', "time now:", str(datetime.now()), "  ",
+                          "time msg:", str(message.get('time'))]))
 
 
 def sender_tlg(number, state):
